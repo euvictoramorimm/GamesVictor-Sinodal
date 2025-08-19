201978a479221e92458edd54805e444f81985544
Victor de Amorim Rodrigues <euvictoramorimm@users.noreply.github.com>
2025-08-19 00:09:03 -0300

    Adicionando a MVC

---
+++ Games.Victor/Games/src/main/java/com/sinodal/Games/GamesVictorApplication.java
@@ -0,0 +1,13 @@
+package com.sinodal.Games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesVictorApplication.class, args);
+	}
+
+}
---
+++ Games.Victor/Games/src/test/java/com/sinodal/Games/GamesVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodal.Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
